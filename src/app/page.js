c6f9d5985919dd80df7f6adea0997bfdd1233782
Kevin Rosario <29806319+KevinRosario01@users.noreply.github.com>
2024-07-30 05:13:03 +0000

    Updated Code

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,38 @@
-import Componets from "./componets/Componets";
+import { Signout } from "@/components/Signout";
+import { createClient } from "../utils/supabase/server";
 
-export default function Home() {
+export default async function Home() {
+  const supabase = createClient();
+  const { data } = await supabase.auth.getUser();
+  //let { data: branches, error } = await supabase.from("branches").select("*");
+
   return (
-    <div className="container mx-auto text-xl"><Componets /></div>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-6">
+      <div className="max-w-md w-full bg-white shadow-lg rounded-lg p-6">
+        <h1 className="text-3xl font-bold text-gray-900 mb-4">Welcome</h1>
+        <p className="text-lg text-gray-800">
+          Hello, <span className="font-semibold text-gray-900">{data?.user?.email}</span>
+          <Signout user={data.user} />
+        </p>
+      </div>
+    </div>
   );
 }
+
+
+/*export default async function Home() {
+  const supabase = createClient();
+  const { data } = await supabase.auth.getUser();
+  let { data: branches, error } = await supabase.from("bracnhes").select("*");
+
+  return (
+    <div className="container mx-auto text-xl">
+      Hello - {data?.user.email}
+      <ul>
+        {branches?.map((branch) => (
+          <li>{branch.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}*/
